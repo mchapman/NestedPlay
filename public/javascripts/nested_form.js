5f5bb7204a3af5acae48d37689297290bf7ee9e3
Mark Chapman <mark.chapman@gmail.com>
2011-05-03 17:51:35 -0400

    Add editing capability

--- public/javascripts/nested_form.js
+++ public/javascripts/nested_form.js
@@ -36,21 +36,20 @@ NestedForm.value = function (fieldsDiv, fieldName) {
     return fieldsDiv.find('input[name$="[' + fieldName + ']"]').val();
 };
 
-$(function() {
-    $('form').delegate('a.add_nested_fields', 'click', function() {
-
+$(function () {
+    $('form').delegate('a.add_nested_fields', 'click', function () {
         // Setup
         var assoc = $(this).attr('data-association');           // Name of child
         var edit_div_id = assoc + '-nested-fields-edit';
 
         // check we don't already have an edit "window"
-        if ($('#' + edit_div_id).length == 0) {
-            $(this).before('<div id="' + edit_div_id + '">' + NestedForm.createDivFromBlueprint(assoc, 'edit') + '</div>');
+        if ($('#' + edit_div_id).length === 0) {
+            $(this).after('<div id="' + edit_div_id + '">' + NestedForm.createDivFromBlueprint(assoc, 'edit') + '</div>');
         }
         return false;
     });
 
-    $('form').delegate('a.remove_nested_fields', 'click', function() {
+    $('form').delegate('a.remove_nested_fields', 'click', function () {
         var hidden_field = $(this).prev('input[type=hidden]')[0];
         if (hidden_field) {
             hidden_field.value = '1';
@@ -64,39 +63,74 @@ $(function() {
         return false;
     });
 
-    $('form').delegate('a.stash_nested_fields', 'click', function() {
+    $('form').delegate('a.edit_nested_fields', 'click', function () {
+        // Setup
+        var assoc = $(this).attr('data-association');           // Name of child
+        var edit_div_id = assoc + '-nested-fields-edit';
+        var edit_popup = $('#'+edit_div_id);
+
+        // Show the pop-up (if not already there)
+        if (edit_popup.length === 0) {
+            $(this).after('<div id="' + edit_div_id + '">' + NestedForm.createDivFromBlueprint(assoc, 'edit') + '</div>');
+            edit_popup = $('#'+edit_div_id);
+        }
+
+        // Populate the popup
+        // find the edit "popup" window
+        var inputs = edit_popup.find('input');
+        var fields = $(this).closest('.fields');
+        // Copy the data from the hidden fields into it
+        inputs.each(function () {
+            var field_name = $(this).attr("name").match(/\[(\w+)\]$/)[1];
+            $(this).val(fields.find('input[name$="[' + field_name + ']"]').val());
+        });
+        // Record the record id
+        edit_popup.data('id', fields.find('input[name$="[id]"]').val());
+
+        return false;
+    });
+
+
+    $('form').delegate('a.stash_nested_fields', 'click', function () {
         // find the edit "popup" window
         var assoc = $(this).attr('data-association');
         var edit_popup = $('#' + assoc + '-nested-fields-edit');
         // see if there is any data in it
         var inputs = edit_popup.find('input');
-        var has_data = false;
-        // See if there is some data there (I am sure this can be improved by someone who knows jQuery / javascript)
-        inputs.each(function() {
-            if ($(this).val() != "") {
-                has_data = true;
-                return false;
-            }
-        });
-        if (has_data) {
-            // create a new view div
-            $('#' + assoc + '-list').append(NestedForm.createDivFromBlueprint(assoc, 'view'));
-            // and select it
-            var new_view = $('#' + assoc + '-list .fields').last();
-            // put the data from the view window into the new view div
-            inputs.each(function() {
-                var field_name = $(this).attr("name").match(/\[(\w+)\]$/)[1];
-                new_view.find('input[name$="[' + field_name + ']"]').val($(this).val());
+        var fields;
+        var id = edit_popup.data('id');
+        if (id) {
+            fields = $('#' + assoc + '-list .fields input[name$="[id]"][value="' + id + '"]').parent();
+        } else {
+            // See if there is some data there (I am sure this can be improved by someone who knows jQuery / javascript)
+            var has_data = false;
+            inputs.each(function () {
+                if ($(this).val() !== "") {
+                    has_data = true;
+                    return false;
+                }
             });
-            // generate the output for the view div
-            $('#' + assoc + '-list').trigger('updateDisplay');
+            if (has_data) {
+                // create a new view div
+                $('#' + assoc + '-list').append(NestedForm.createDivFromBlueprint(assoc, 'view'));
+                // and select it
+                fields = $('#' + assoc + '-list .fields').last();
+            }
         }
+        // put the data from the pop-up window into the new view div
+        inputs.each(function () {
+            var field_name = $(this).attr("name").match(/\[(\w+)\]$/)[1];
+            fields.find('input[name$="[' + field_name + ']"]').val($(this).val());
+        });
+        // generate the output for the view div
+        $('#' + assoc + '-list').trigger('updateDisplay');
+
         // Lose the popup window
         edit_popup.remove();
         return false;
     });
 
-    $('form').delegate('a.cancel_nested_fields', 'click', function() {
+    $('form').delegate('a.cancel_nested_fields', 'click', function () {
         // remove the edit "popup" window
         var assoc = $(this).attr('data-association');
         var edit_popup = $('#' + assoc + '-nested-fields-edit');
